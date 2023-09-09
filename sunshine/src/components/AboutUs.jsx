07448d8d5c663888f990a31c58bf125b6cd45fd5
Mohityadav-code <mohityadav3552@gmail.com>
2023-09-09 11:29:41 +0530

    Update About Us

--- sunshine/src/components/AboutUs.jsx
+++ sunshine/src/components/AboutUs.jsx
@@ -8,10 +8,34 @@ export default function AboutUs() {
           <div className="text-5xl py-5 pb-7 underline underline-offset-4 text-center font-sans font-black text-[#222831] w-full">
             About Us
           </div>
-       
-          <div className="text-[#7a7a7a] my-6 text-[17px] h-96  mx-3 lg:mx-32 md:mx-32 leading-relaxed font-[400]">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eius, tempora aspernatur eligendi, ipsam molestias possimus minus quos, dolor neque corrupti nemo esse aliquid est reprehenderit autem sunt praesentium et.
 
+          <div className="text-[#7a7a7a] mt-6 text-[17px] text-justify   mx-3 lg:mx-16 md:mx-16 leading-relaxed font-[400]">
+            Welcome to Sunshine Budokan Karate Dojos! We are proud to be the
+            premier martial arts training center located in the picturesque town
+            of Karinkal, nestled within the vibrant region of Kanyakumari, Tamil
+            Nadu, India. Our mission is dedicated to providing top-notch Karate
+            training to students of all age groups and skill levels, fostering
+            discipline, strength, and a deep appreciation for martial arts.
+          </div>
+          <div className="text-[#7a7a7a] mt-6 text-[17px]  text-justify  mx-3 lg:mx-16 md:mx-16 leading-relaxed font-[400]">
+            At Sunshine Budokan, our experienced and certified instructors bring
+            a wealth of knowledge and a passion for Karate, ensuring that every
+            student receives the highest quality instruction. Whether you're a
+            beginner taking your first steps on the martial arts journey or an
+            advanced practitioner honing your skills, our dojo is the perfect
+            place to grow and achieve your goals.
+          </div>
+
+          <div className="text-[#7a7a7a]  text-[17px] my-6 pb-6  text-justify  mx-3 lg:mx-16 md:mx-16 leading-relaxed font-[400]">
+            <span className=" font-bold">Our Legacy</span>
+            With a legacy spanning numerous years, Sunshine Budokan Karate Dojos
+            stands as a prominent figure in martial arts education within our
+            community. Our dedicated instructors, deeply committed to the art of
+            karate, bring their extensive expertise to our dojo, ensuring
+            top-notch instruction. We take immense pride in upholding the
+            esteemed heritage of this martial discipline, passing on its
+            time-honored traditions to our students with unwavering dedication
+            and enthusiasm.
           </div>
         </div>
       </div>
--- sunshine/src/components/IconsLayout.jsx
+++ sunshine/src/components/IconsLayout.jsx
@@ -1,27 +1,41 @@
-import React from 'react'
+import React from "react";
 
 export default function IconsLayout() {
   return (
-    <div className=' w-full h-full pb-10 bg-[#F8F9F9]'>
-
-<div className="text-5xl py-5 mt-5 underline underline-offset-4 text-center font-sans font-black text-[#222831] w-full">
-ICONS OF KBI
-    </div>
-    <div className=" w-full h-full  flex justify-around items-center ">
-        <div className='  justify-center items-center flex  w-[400px] h-full py-10 '>
-
-       <img src="https://lh3.googleusercontent.com/p/AF1QipPJWoiRGwO7x0kPvmnxJhsjFc5BfqEzp-z4BJNs=w600-h0" className=' bg-red-400 p-2' alt="" />
+    <div className=" w-full h-full pb-10 bg-[#F8F9F9]">
+      <div className="text-5xl py-5 mt-5 underline underline-offset-4 text-center font-sans font-black text-[#222831] w-full">
+        ICONS OF KBI
+      </div>
+      <div className=" w-full h-full gap-4  flex justify-around items-center ">
+        <div className="  justify-center items-center flex  w-[400px] h-full py-10 ">
+          <img
+            src="https://lh3.googleusercontent.com/p/AF1QipPJWoiRGwO7x0kPvmnxJhsjFc5BfqEzp-z4BJNs=w600-h0"
+            className="  "
+            alt=""
+          />
         </div>
-        <div className='  justify-center items-center flex  w-[400px] h-full py-10'>
-
-       <img src="https://lh3.googleusercontent.com/p/AF1QipOtMFD03EErdTiehHZn9ZGHmUsLH6rIf2rrLcFe=w600-h0" className=' bg-red-400 p-2' alt="" />
+        <div className="  justify-center items-center flex  w-[400px] h-full py-10">
+          <img
+            src="https://lh3.googleusercontent.com/p/AF1QipOtMFD03EErdTiehHZn9ZGHmUsLH6rIf2rrLcFe=w600-h0"
+            className="  "
+            alt=""
+          />
         </div>
-        <div className='  justify-center items-center flex  w-[400px] h-full py-10'>
-
-       <img src="https://lh3.googleusercontent.com/p/AF1QipO-VguPUOU1XFR_lKNIIOc6UB-n0Q_-rGaut2sh=w600-h0" className=' bg-red-400 p-2' alt="" />
+        <div className="  justify-center items-center flex  w-[400px] h-full py-10">
+          <img
+            src="https://lh3.googleusercontent.com/p/AF1QipO-VguPUOU1XFR_lKNIIOc6UB-n0Q_-rGaut2sh=w600-h0"
+            className="  "
+            alt=""
+          />
         </div>
-        
+        <div className="  justify-center items-center flex  w-[400px] h-full py-10">
+          <img
+            src="https://lh3.googleusercontent.com/p/AF1QipO-VguPUOU1XFR_lKNIIOc6UB-n0Q_-rGaut2sh=w600-h0"
+            className=" "
+            alt=""
+          />
+        </div>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
